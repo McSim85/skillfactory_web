86af2182d89ce40644aa71fcd0c931a005c48f9b
Mc.Sim <32900310+McSim85@users.noreply.github.com>
2019-07-25 00:31:18 +0300

    A5.15 prepare2

---
+++ A5/А5.15-practice/js/script.js
@@ -0,0 +1,19 @@
+﻿const grandpa = "Дед"
+const grandma = "Баба"
+
+$(document).ready(function() {
+	$(".btn").click(function() {
+		const result = $(".result");
+
+		$.getJSON("https://api.myjson.com/bins/jcmhn", function (data) {
+		  console.log(data.text);
+		  data.text.var1 = "123"
+		  //$(result).attr('var1', grandpa)
+		  $(result).html('<p>' + data.text + '</p>');
+		});
+	});
+});
+
+
+
+
