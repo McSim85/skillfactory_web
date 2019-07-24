05b5105f117b31e05f289fd001f3fa83c29fccc8
Mc.Sim <32900310+McSim85@users.noreply.github.com>
2019-07-24 15:16:49 +0300

    A5.13 prepare A5.11 done

---
+++ A5/А5.11-jQuery/js/script.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function() {
+	const blockWithId = $('#root'); // ищет по параметру id
+	const blockWithClass = $('.block'); // ищет по параметру class
+	const spanElement = $('span') // ищет по имени тега
+
+	console.log(blockWithId);
+	console.log(blockWithClass);
+	console.log(spanElement);
+});
