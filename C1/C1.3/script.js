495437d006db46b4e36eabe6b8dabb3c10d2ce39
Mc.Sim <32900310+McSim85@users.noreply.github.com>
2019-09-28 21:44:28 +0300

    C1.8 has done

---
+++ C1/C1.3/script.js
@@ -0,0 +1,7 @@
+﻿const content = document.querySelector(".content");
+
+function getRandomDog (breed){
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+}
+
+getRandomDog('briard') // любая другая порода из списка со строчной буквы
