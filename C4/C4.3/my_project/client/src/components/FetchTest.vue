d17a2c24d515c3266157fee2a212c275a5c9c787
McSim85 <maxim@kramarenko.pro>
2019-10-31 23:57:23 +0300

    start build VUE

---
+++ C4/C4.3/my_project/client/src/components/FetchTest.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+const dataURL = 'https://api.myjson.com/bins/10dxnx';
+
+export default {
+  name: 'Fetch',
+  data() {
+    return {
+      msg: 'Before request',
+    };
+  },
+  created() {
+    axios.get(dataURL)
+      .then((response) => {
+        console.log(response);
+        this.msg = response.data.message;
+      });
+  },
+};
+</script>
